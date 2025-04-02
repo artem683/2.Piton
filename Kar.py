b991babd4f884274d52d552a3f72d6da825e45e2
artem683 <sea796497@gmail.com>
2025-04-02 14:28:39 +0300

    Update Kar.py

--- Kar.py
+++ Kar.py
@@ -18,3 +18,35 @@ car = Car("киа","спортейдж","20",500)
 car.get_info()
 car.drive(200)
 car.get_info()
+
+class Laptop:
+    def __init__(self,brand,ram,battery):
+        self.brand=brand
+        self.ram=ram
+        self.battery=battery
+    def upgrade_ram (self,GB):
+        self.ram += GB
+
+    def se_batteryu (self,Proc):
+        self.battery -=Proc
+    def get_info(self):
+        print(f"{self.brand}\n"
+              f"{self.ram}\n"
+              f"{self.battery}\n")
+l1 = Laptop ("Lenovo", 300, 15)
+l1.get_info()
+l1.upgrade_ram(80)
+l1.se_batteryu(9)
+l1.get_info()
+
+l2 = Laptop("Apple",500,50)
+l2.get_info()
+l2.upgrade_ram(990)
+l2.se_batteryu(16)
+l2.get_info()
+
+l3 = Laptop("ASUS",560,70)
+l3.get_info()
+l3.upgrade_ram(33)
+l3.se_batteryu(20)
+l3.get_info()
